c298a35bf14842b801aa02c092c59fbbde7c879e
Thiago Veras <thiago.veras.machado@gmail.com>
2020-12-22 18:59:34 +0000

    Automatic brew bump version

--- Formula/vtex.rb
+++ Formula/vtex.rb
@@ -1,15 +1,9 @@
 class Vtex < Formula
   desc "CLI tool for creating and managing VTEX apps"
   homepage "https://github.com/vtex/toolbelt"
-<<<<<<< HEAD
   url "https://vtex-toolbelt-test.s3.amazonaws.com/vtex-v2.121.0/vtex-v2.121.0-darwin-x64.tar.gz"
   version "2.121.0"
   sha256 "fe977ac320cafab6bfa748206506d69e09b47e0451845c5522336adaf3ab23bb"
-=======
-  url "https://vtex-toolbelt-test.s3.amazonaws.com/vtex-v2.119.2/vtex-v2.119.2-darwin-x64.tar.gz"
-  version "2.119.2"
-  sha256 "e8d41f7d45f757e8ae2dea4fef909bbc38bd9e405a68ea86ae47da89f3aa4df2"
->>>>>>> Automatic brew bump version
   version "2.119.4"
 
   def install
