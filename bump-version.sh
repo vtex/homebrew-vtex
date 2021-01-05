523b6c22ad9617de333cd7a8462e273df33515fa
Thiago Veras <thiago.veras.machado@gmail.com>
2021-01-05 17:47:00 -0300

    toolbelt-notification integration (#6)
    * Automatic brew bump version
    
    * Automatic brew bump version
    
    * Add toolbelt-notification call
    
    * Add toolbelt-notification token
    
    * Change commit messafe

--- bump-version.sh
+++ bump-version.sh
@@ -16,6 +16,7 @@ version=$(echo $allData | jq --raw-output '.version')
 url=$(echo $allData | jq --raw-output '.gz')
 sha256=$(echo $allData | jq --raw-output '.sha256gz')
 
+echo "Latest verison: $version"
 url="  url \"$url\""
 version="  version \"$version\""
 sha256="  sha256 \"$sha256\""
