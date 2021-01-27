56d6db1f596d23bd7b2230a774c2b0e5f696eef3
Thiago Veras <thiago.veras.machado@gmail.com>
2021-01-27 21:47:20 +0000

    [Automatic] Release v2.121.2

--- Formula/vtex.rb
+++ Formula/vtex.rb
@@ -1,9 +1,9 @@
 class Vtex < Formula
   desc "CLI tool for creating and managing VTEX apps"
   homepage "https://github.com/vtex/toolbelt"
-  url "https://vtex-toolbelt-test.s3.amazonaws.com/vtex-v2.121.2/vtex-v2.121.2-darwin-x64.tar.gz"
-  version "2.121.2"
-  sha256 "410baa95cc15ef457c80b87b000f9755d53d1c492226a0bdb921a1a2389d0064"
+  url "https://vtex-toolbelt-test.s3.amazonaws.com/vtex-v2.121.1/vtex-v2.121.1-darwin-x64.tar.gz"
+  version "2.121.1"
+  sha256 "4e24de68f947ce14379110c36ea8e3c1aa32c0c3bcceab09b533c15a58f04fd8"
   version "2.119.4"
 
   def install
